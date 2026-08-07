76f13592141865cbe8e0ccc708edf1bcc0d41af1
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-08-06 22:14:35 -0300

    add provider to global config
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,14 @@
 import type { ApplicationConfig } from '@angular/core';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withViewTransitions,
-} from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
+import { albumRepositoryProvider } from './core/providers/album-repository.provider';
+import { HttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes,
-      withComponentInputBinding(),
-      withViewTransitions(),
-    ),
+    albumRepositoryProvider,
+    HttpClient,
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
   ],
 };
