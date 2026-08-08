ca712f48a28bb8d5008dc7beb8c9e88e420403e5
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-08-08 16:08:20 -0300

    config: fix build path
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mudo.app',
   appName: 'album-app',
-  webDir: 'dist'
+  webDir: 'dist/album-app/browser',
 };
 
 export default config;
